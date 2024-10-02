e004c26c42a60b9fd73ca5e797de5e9831121ad8
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-02 08:50:26 +0100

    up-to-sync

---
+++ src/MoodNft.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
+    constructor(
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("MOOD NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        happySvg =s_happySvg;
+    }
+
+    function mintNft()public{
+        _safeMint(msg.sender,s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory){}
+}
